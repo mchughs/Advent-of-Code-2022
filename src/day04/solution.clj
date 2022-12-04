f6a73271d6193f7970bb9621b7f6fbbd56b921a2
mchughs <smchugh230395@gmail.com>
2022-12-04 11:06:42 +0100

    day 4 solution

--- src/day04/solution.clj
+++ src/day04/solution.clj
@@ -1,1 +1,40 @@
-(ns day04.solution)
+(ns day04.solution
+  (:require [clojure.java.io :as io]))
+
+(def ^:private parse-values
+  (comp (partial map read-string)
+        (partial re-seq #"\d+")))
+
+(defn- fully-contained? [[start-1 end-1 start-2 end-2]]
+  (if (or (<= start-1 start-2 end-2 end-1)
+          (<= start-2 start-1 end-1 end-2))
+    1
+    0))
+
+;; solution part 1
+
+(time
+ (with-open [rdr (io/reader "src/day04/input.txt")]
+   (->> rdr
+        line-seq
+        (transduce
+         (map (comp fully-contained? parse-values))
+         +))))
+
+;; solution part 2
+
+(defn- any-overlap? [[start-1 end-1 start-2 end-2]]
+  (if (or (<= start-1 start-2 end-1 end-2)
+          (<= start-2 start-1 end-2 end-1)
+          (<= start-1 start-2 end-2 end-1)
+          (<= start-2 start-1 end-1 end-2))
+    1
+    0))
+
+(time
+ (with-open [rdr (io/reader "src/day04/input.txt")]
+   (->> rdr
+        line-seq
+        (transduce
+         (map (comp any-overlap? parse-values))
+         +))))
